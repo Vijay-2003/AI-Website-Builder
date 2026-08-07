a609ecfcd9e652c3a818a660df9f1d45b015aff6
Vijay Digambar Kusekar <vijay.kusekar2003@gmail.com>
2026-08-07 22:36:56 +0530

    backend server created with authentication feature

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { login, logout, me, register } from '../controllers/authController.js';
+import { authMiddleware } from '../middleware/authMiddleware.js';
+
+const authRouter = express.Router();
+
+authRouter.post("/register", register);
+authRouter.post("/login", login);
+authRouter.post("/logout", logout);
+authRouter.get("/me", authMiddleware, me);
+
+export default authRouter;
